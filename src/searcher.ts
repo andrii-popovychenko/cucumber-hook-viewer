0bba97ca001352437f132e1c8db65747068a0ace
Andrii Popovychenko <andrii-popovychenko@gmail.com>
2021-07-16 17:42:22 +0300

    Implemented refresh feature for data tree

--- src/searcher.ts
+++ src/searcher.ts
@@ -1,26 +1,27 @@
 import * as vscode from 'vscode';
-import { WorkspaceFolder, RelativePattern } from 'vscode';
+import { RelativePattern } from 'vscode';
 import Constants from './constants';
 
 export class Searcher {
 
-    private workspaceFolder: WorkspaceFolder;
+    constructor() {}
 
-    constructor() {
+    private getWorkspaceFolder = () => {
         if (vscode.workspace.workspaceFolders?.length) {
-            this.workspaceFolder = vscode.workspace.workspaceFolders[0];
+            return vscode.workspace.workspaceFolders[0];
         } else {
             throw new Error('You need to set up workspace!');
         }
-    }
+    };
 
     readFolderPath = () => vscode.workspace.getConfiguration(Constants.extensionName).get(Constants.pathToHookFolder) as string;
 
     isSettingExist = () => vscode.workspace.getConfiguration(Constants.extensionName).has(Constants.pathToHookFolder);
 
     findFiles = () => {
+        const workspaceFolder = this.getWorkspaceFolder();
         const folderPath = this.readFolderPath();
-        const searchPattern = new RelativePattern(this.workspaceFolder, `${folderPath}/*.ts`);
+        const searchPattern = new RelativePattern(workspaceFolder, `${folderPath}/*.ts`);
         return vscode.workspace.findFiles(searchPattern.pattern);
     };
 
